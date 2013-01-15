8eeb938f7e8ae4088d2f5866dd560e58d0840e15
nyomo <nyomoko@gmail.com>
2013-01-15 23:46:27 +0900

    ファイルの内容が全部必要なので移動した

--- sample.php
+++ sample.php
@@ -1,5 +1,7 @@
 <?php
 require_once("func.php");
+require_once("plugin.php");
+require_once("make_link.php");
 require_once("convert_html.php");
 $lines = array("test","*test","[[hoge]]","+リスト","+list","''hoge''");
 $html = convert_html($lines);
