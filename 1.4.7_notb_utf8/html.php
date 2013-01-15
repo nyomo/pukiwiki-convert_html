65ffb0654b3715f5069f0e22005dcbb9d4e76211
nyomo <nyomoko@gmail.com>
2013-01-15 23:50:39 +0900

    必要な関数を移動

--- 1.4.7_notb_utf8/html.php
+++ 1.4.7_notb_utf8/html.php
@@ -318,44 +318,6 @@ function make_related($page, $tag = '')
 	return $retval;
 }
 
-// User-defined rules (convert without replacing source)
-function make_line_rules($str)
-{
-	global $line_rules;
-	static $pattern, $replace;
-
-	if (! isset($pattern)) {
-		$pattern = array_map(create_function('$a',
-			'return \'/\' . $a . \'/\';'), array_keys($line_rules));
-		$replace = array_values($line_rules);
-		unset($line_rules);
-	}
-
-	return preg_replace($pattern, $replace, $str);
-}
-
-// Remove all HTML tags(or just anchor tags), and WikiName-speific decorations
-function strip_htmltag($str, $all = TRUE)
-{
-	global $_symbol_noexists;
-	static $noexists_pattern;
-
-	if (! isset($noexists_pattern))
-		$noexists_pattern = '#<span class="noexists">([^<]*)<a[^>]+>' .
-			preg_quote($_symbol_noexists, '#') . '</a></span>#';
-
-	// Strip Dagnling-Link decoration (Tags and "$_symbol_noexists")
-	$str = preg_replace($noexists_pattern, '$1', $str);
-
-	if ($all) {
-		// All other HTML tags
-		return preg_replace('#<[^>]+>#',        '', $str);
-	} else {
-		// All other anchor-tags only
-		return preg_replace('#<a[^>]+>|</a>#i', '', $str);
-	}
-}
-
 // Remove AutoLink marker with AutLink itself
 function strip_autolink($str)
 {
@@ -372,28 +334,6 @@ function make_search($page)
 
 	return '<a href="' . $script . '?plugin=related&amp;page=' . $r_page .
 		'">' . $s_page . '</a> ';
-}
-
-// Make heading string (remove heading-related decorations from Wiki text)
-function make_heading(& $str, $strip = TRUE)
-{
-	global $NotePattern;
-
-	// Cut fixed-heading anchors
-	$id = '';
-	$matches = array();
-	if (preg_match('/^(\*{0,3})(.*?)\[#([A-Za-z][\w-]+)\](.*?)$/m', $str, $matches)) {
-		$str = $matches[2] . $matches[4];
-		$id  = & $matches[3];
-	} else {
-		$str = preg_replace('/^\*{0,3}/', '', $str);
-	}
-
-	// Cut footnotes and tags
-	if ($strip === TRUE)
-		$str = strip_htmltag(make_link(preg_replace($NotePattern, '', $str)));
-
-	return $id;
 }
 
 // Separate a page-name(or URL or null string) and an anchor
---
+++ html.php
@@ -0,0 +1,70 @@
+<?php
+// PukiWiki - Yet another WikiWikiWeb clone.
+// $Id: html.php,v 1.57 2006/04/15 17:33:35 teanan Exp $
+// Copyright (C)
+//   2002-2006 PukiWiki Developers Team
+//   2001-2002 Originally written by yu-ji
+// License: GPL v2 or (at your option) any later version
+//
+// HTML-publishing related functions
+
+// User-defined rules (convert without replacing source)
+function make_line_rules($str)
+{
+	global $line_rules;
+	static $pattern, $replace;
+
+	if (! isset($pattern)) {
+		$pattern = array_map(create_function('$a',
+			'return \'/\' . $a . \'/\';'), array_keys($line_rules));
+		$replace = array_values($line_rules);
+		unset($line_rules);
+	}
+
+	return preg_replace($pattern, $replace, $str);
+}
+
+// Make heading string (remove heading-related decorations from Wiki text)
+function make_heading(& $str, $strip = TRUE)
+{
+	global $NotePattern;
+
+	// Cut fixed-heading anchors
+	$id = '';
+	$matches = array();
+	if (preg_match('/^(\*{0,3})(.*?)\[#([A-Za-z][\w-]+)\](.*?)$/m', $str, $matches)) {
+		$str = $matches[2] . $matches[4];
+		$id  = & $matches[3];
+	} else {
+		$str = preg_replace('/^\*{0,3}/', '', $str);
+	}
+
+	// Cut footnotes and tags
+	if ($strip === TRUE)
+		$str = strip_htmltag(make_link(preg_replace($NotePattern, '', $str)));
+
+	return $id;
+}
+
+// Remove all HTML tags(or just anchor tags), and WikiName-speific decorations
+function strip_htmltag($str, $all = TRUE)
+{
+	global $_symbol_noexists;
+	static $noexists_pattern;
+
+	if (! isset($noexists_pattern))
+		$noexists_pattern = '#<span class="noexists">([^<]*)<a[^>]+>' .
+			preg_quote($_symbol_noexists, '#') . '</a></span>#';
+
+	// Strip Dagnling-Link decoration (Tags and "$_symbol_noexists")
+	$str = preg_replace($noexists_pattern, '$1', $str);
+
+	if ($all) {
+		// All other HTML tags
+		return preg_replace('#<[^>]+>#',        '', $str);
+	} else {
+		// All other anchor-tags only
+		return preg_replace('#<a[^>]+>|</a>#i', '', $str);
+	}
+}
+?>
