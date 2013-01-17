60e7cc84d206ccca15585eeddde5b3aafbdefbae
nyomo <nyomoko@gmail.com>
2013-01-17 12:18:39 +0900

    csv_explode関数が必要になったので持ってきた

--- 1.4.7_notb_utf8/func.php
+++ 1.4.7_notb_utf8/func.php
@@ -607,25 +607,6 @@ function sanitize($param) {
 	return input_filter($param);
 }
 
-// Explode Comma-Separated Values to an array
-function csv_explode($separator, $string)
-{
-	$retval = $matches = array();
-
-	$_separator = preg_quote($separator, '/');
-	if (! preg_match_all('/("[^"]*(?:""[^"]*)*"|[^' . $_separator . ']*)' .
-	    $_separator . '/', $string . $separator, $matches))
-		return array();
-
-	foreach ($matches[1] as $str) {
-		$len = strlen($str);
-		if ($len > 1 && $str{0} == '"' && $str{$len - 1} == '"')
-			$str = str_replace('""', '"', substr($str, 1, -1));
-		$retval[] = $str;
-	}
-	return $retval;
-}
-
 // Implode an array with CSV data format (escape double quotes)
 function csv_implode($glue, $pieces)
 {
--- func.php
+++ func.php
@@ -71,4 +71,23 @@ function strip_bracket($str)
 	}
 }
 
+// Explode Comma-Separated Values to an array
+function csv_explode($separator, $string)
+{
+	$retval = $matches = array();
+
+	$_separator = preg_quote($separator, '/');
+	if (! preg_match_all('/("[^"]*(?:""[^"]*)*"|[^' . $_separator . ']*)' .
+	    $_separator . '/', $string . $separator, $matches))
+		return array();
+
+	foreach ($matches[1] as $str) {
+		$len = strlen($str);
+		if ($len > 1 && $str{0} == '"' && $str{$len - 1} == '"')
+			$str = str_replace('""', '"', substr($str, 1, -1));
+		$retval[] = $str;
+	}
+	return $retval;
+}
+
 ?>
