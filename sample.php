0806b899ea6326314661ff17bfa66435164ec943
nyomo <nyomoko@gmail.com>
2013-02-08 14:24:54 +0900

    プラグインの動作サンプルを追加

--- sample.php
+++ sample.php
@@ -2,7 +2,7 @@
 require_once("conf.php");
 require_once("convert_html.php");
 $vars = array('page'=>'hoge');
-$lines = array("test","*test","[[hoge]]","+リスト","+list","''hoge''");
+$lines = array("test","*test","[[hoge]]","+リスト","+list","''hoge''","#includex(hoge,off)","#hr()");
 $html = convert_html($lines);
 echo $html;
 ?>
